1c581c1fdb9b56b752c4c3122a7b6f0938f3a879
Letizia DI MAIO <109817389+leta91@users.noreply.github.com>
2024-01-22 16:49:40 +0000

    add code in functions.php

--- functions.php
+++ functions.php
@@ -1,22 +1,115 @@
 <?php
+/**
+ * Functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package agrippa
+ * @since 1.0.0
+ */
 
+ /**
+ * The theme version.
+ *
+ * @since 1.0.0
+ */
+define( 'AGRIPPA_VERSION', wp_get_theme()->get( 'Version' ) );
+
+/**
+ * Add theme support for block styles and editor style.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function agrippa_setup() {
+	add_editor_style( './assets/css/style-shared.min.css' );
+
+	/*
+	 * Load additional block styles.
+	 */
+	$styled_blocks = [ 'button', 'quote', 'navigation', 'search' ];
+	foreach ( $styled_blocks as $block_name ) {
+		$args = array(
+			'handle' => "agrippa-$block_name",
+			'src'    => get_theme_file_uri( "assets/css/blocks/$block_name.min.css" ),
+			'path'   => get_theme_file_path( "assets/css/blocks/$block_name.min.css" ),
+		);
+		// Replace the "core" prefix if you are styling blocks from plugins.
+		wp_enqueue_block_style( "core/$block_name", $args );
+	}
+
+}
+add_action( 'after_setup_theme', 'agrippa_setup' );
 
 // Load style.css on the front-end through the function wp_enqueue_scripts
-function agrippa_enqueue_styles() {
-	wp_enqueue_style( 
-		'agrippa-style', 
-		get_stylesheet_uri()    /* ->Returns the active theme’s style.css file URL*/
+
+/**
+ * Enqueue the CSS files.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+
+function agrippa_styles() {
+	wp_enqueue_style(
+		'agrippa-style',
+		get_stylesheet_uri(),  /* ->Returns the active theme’s style.css file URL*/
+		[],
+		AGRIPPA_VERSION
+	);
+	wp_enqueue_style(
+		'agrippa-shared-styles',
+		get_theme_file_uri( 'assets/css/style-shared.min.css' ),
+		[],
+		AGRIPPA_VERSION
 	);
 }
+add_action( 'wp_enqueue_scripts', 'agrippa_styles' );
 
-add_action( 'wp_enqueue_scripts', 'agrippa_enqueue_styles' );
+// Filters.
+require_once get_theme_file_path( 'inc/filters.php' );
+
+// Block variation example.
+require_once get_theme_file_path( 'inc/register-block-variations.php' );
+
+// Block style examples.
+require_once get_theme_file_path( 'inc/register-block-styles.php' );
+
+// Block pattern and block category examples.
+require_once get_theme_file_path( 'inc/register-block-patterns.php' );
 
 
-// Allow the custom CSS in style.css to load in the editor  
-function agrippa_setup() {
-    add_editor_style( get_stylesheet_uri() );
-}
+// Allow SVG images to be uploaded
 
-add_action( 'after_setup_theme', 'agrippa_setup' );
-    
+add_filter( 'wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
+	$filetype = wp_check_filetype( $filename, $mimes );
+	return [
+		'ext'             => $filetype['ext'],
+		'type'            => $filetype['type'],
+		'proper_filename' => $data['proper_filename']
+	];
+  
+  }, 10, 4 );
+  
+  function cc_mime_types( $mimes ){
+	$mimes['svg'] = 'image/svg+xml';
+	return $mimes;
+  }
+  add_filter( 'upload_mimes', 'cc_mime_types' );
+  
+  function fix_svg() {
+	echo '<style type="text/css">
+		  .attachment-266x266, .thumbnail img {
+			   width: 100% !important;
+			   height: auto !important;
+		  }
+		  </style>';
+  }
+  add_action( 'admin_head', 'fix_svg' );
 
+
+
+
+			
